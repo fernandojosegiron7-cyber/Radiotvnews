c8124b092a9b3b687308183b30ff89ffa2e7afc7
fernandojosegiron7-cyber <fernandojosegiron7@gmail.com>
2026-08-20 09:31:17 -0600

    Add files via upload

---
+++ api/logout.js
@@ -0,0 +1,6 @@
+const { clearSessionCookie } = require("./_lib/auth");
+module.exports = async (req, res) => {
+  if (req.method !== "POST") return res.status(405).json({ error: "Método no permitido" });
+  res.setHeader("Set-Cookie", clearSessionCookie());
+  res.status(200).json({ ok: true });
+};
